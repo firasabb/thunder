ee59748d97c1ea3f8ffe1dc340e62b6738ac1df7
Firas Abbas <fabbas@rismedia.com>
2024-07-02 20:07:37 -0400

    1.0.2

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -15,18 +15,20 @@ class TeamController extends Controller
         if($conference && $conference != 'all'){
 
             $conference = Conference::where('abbreviation', $conference)->firstOrFail();
-            $teams = $conference->teams()->orderBy('name')->get();
+            $teams = $conference->teams();
 
         } else if($conference == 'all'){
 
-            $teams = Team::where('status', 'active')->get();
+            $teams = Team::where('status', 'active');
         }
         else {
             // get the teams where the conferences are not acc, big12, big10, sec
             $teams = Team::where('status', 'active')->whereHas('conferences', function($query){
                 $query->where('abbreviation', 'PAC');
-            })->get();
+            });
         }
+
+        $teams = $teams->orderBy('name')->get();
 
         return response()->json($teams);
     }
